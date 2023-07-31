b1c1016c34fb1c04d95864072e6fa7d6fc2b17ef
pongpatapee <pongpatapee1021@gmail.com>
2023-07-30 17:29:37 -0700

    good enough hyprland config

---
+++ .config/hypr/scripts/desktop_portal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sleep 1
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-wlr
+killall xdg-desktop-portal
+/usr/libexec/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/libexec/xdg-desktop-portal &
+echo "executed desktop portal script"
