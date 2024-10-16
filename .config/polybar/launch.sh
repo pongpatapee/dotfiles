d71551223e0b6a0aacf83588d86bbbfbbff9c2e1
pongpatapee <pongpatapee1021@gmail.com>
2024-10-15 21:23:41 -0400

    configured i3 and polybar for 2 monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,8 +7,16 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main 2>&1 | tee -a /tmp/polybar1.log &
-disown
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar main 2>&1 | tee -a /tmp/polybar1.log &
+# disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
 
 echo "Bars launched..."
